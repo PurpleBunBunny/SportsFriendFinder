824bf18c08eaefba0b480e0170ab4ffcb46c8f82
PurpleBunny <michael.l33.1994@gmail.com>
2018-11-18 20:16:04 -0800

    final commit

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var userData = [
+    {
+      name: "Ahmed",
+      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [
+          1,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3
+        ]
+    }
+  ];
+
+  module.exports = userData;
